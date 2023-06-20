e1da4909b8c93ffc297b4ff0bf259a256e84d95a
Donchess1 <Stepabod@yahoo.com>
2023-06-20 15:20:53 -0700

    all cleared

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,38 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
-  * main-Starts here
-  * decription-prints _putchar
-  * Return:0
-*/
+
+ * main - print _putchar
+
+ * 
+
+ * Return: 0
+
+ */
+
+
 int main(void)
+
 {
-	printf("_putchar\n");
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
 return (0);
+
 }
