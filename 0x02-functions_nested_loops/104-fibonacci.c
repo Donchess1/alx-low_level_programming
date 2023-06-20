3fa99f6d1276ec9fd05f5e5ca66c559ddf088585
Donchess1 <Stepabod@yahoo.com>
2023-06-20 16:40:53 -0700

    victory

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,87 +1,73 @@
 #include <stdio.h>
 
-
 /**
-
- * main - fibonacci <3
-
- *
-
- * Purpose - no hardcode
-
- *
-
- * Return:  (Success)
-
- */
-
+* main - fibonacci <3
+* Purpose - no hardcode
+* Return:  (Success)
+*/
 
 int main(void)
 
 {
 
-        unsigned long int i;
+unsigned long int i;
 
-        unsigned long int bef = 1;
+unsigned long int bef = 1;
 
-        unsigned long int aft = 2;
+unsigned long int aft = 2;
 
-        unsigned long int l = 1000000000;
+unsigned long int l = 1000000000;
 
-        unsigned long int bef1;
+unsigned long int bef1;
 
-        unsigned long int bef2;
+unsigned long int bef2;
 
-        unsigned long int aft1;
+unsigned long int aft1;
 
-        unsigned long int aft2;
+unsigned long int aft2;
 
+printf("%lu", bef);
 
-        printf("%lu", bef);
+for (i = 1; i < 91; i++)
 
+{
 
-        for (i = 1; i < 91; i++)
+printf(", %lu", aft);
 
-        {
+aft += bef;
 
-                printf(", %lu", aft);
+bef = aft - bef;
 
-                aft += bef;
+}
 
-                bef = aft - bef;
+bef1 = (bef / l);
 
-        }
+bef2 = (bef % l);
 
+aft1 = (aft / l);
 
-        bef1 = (bef / l);
+aft2 = (aft % l);
 
-        bef2 = (bef % l);
+for (i = 92; i < 99; ++i)
 
-        aft1 = (aft / l);
+{
 
-        aft2 = (aft % l);
+printf(", %lu", aft1 + (aft2 / l));
 
+printf("%lu", aft2 % l);
 
-        for (i = 92; i < 99; ++i)
+aft1 = aft1 + bef1;
 
-        {
+bef1 = aft1 - bef1;
 
-                printf(", %lu", aft1 + (aft2 / l));
+aft2 = aft2 + bef2;
 
-                printf("%lu", aft2 % l);
+bef2 = aft2 - bef2;
 
-                aft1 = aft1 + bef1;
+}
 
-                bef1 = aft1 - bef1;
+printf("\n");
 
-                aft2 = aft2 + bef2;
-
-                bef2 = aft2 - bef2;
-
-        }
-
-        printf("\n");
-
-        return (0);
+return (0);
 
 }
