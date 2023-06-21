72eb03183feb45d78fbb67b5fb273c19f0d4b9b3
Donchess1 <Stepabod@yahoo.com>
2023-06-20 17:13:28 -0700

    go

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,7 @@ void times_table(void)
 int i, j, k;
 
 for (i = 0; i < 10; i++)
-
 {
-
 for (j = 0; j < 10; j++)
 
 {
@@ -31,24 +29,19 @@ _putchar(k + '0');
 if (k < 10 && j != 0)
 
 {
-
-_putchar(',');
-
-_putchar(' ');
-
-_putchar(' ');
-
-_putchar(k + '0');
-
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(k + '0');
 }
 else if (k >= 10)
 
 {
-
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}}
+	_putchar(',');
+	_putchar(' ');
+	_putchar((k / 10) + '0');
+	_putchar((k % 10) + '0');
+}
+}
 _putchar('\n');
 }}
