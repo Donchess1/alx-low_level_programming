6eca74872fb1f8420f2a9aa9c4c78a62cbf1bd9e
Donchess1 <stepabod@yahoo.com>
2023-10-17 09:09:45 +0000

    da

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -12,11 +12,19 @@ int sub(int a, int b)
 {
 	return (a - b);
 }
-int divide(int a, int b)
+int divi(int a, int b)
 {
+	if (b == 0)
+	{
+		return (0);
+	}
 	return (a / b);
 }
 int mod(int a, int b)
 {
+	if (b == 0)
+	{
+		return (0);
+	}
 	return (a % b);
 }
