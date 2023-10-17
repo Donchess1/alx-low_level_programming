5c789e0f3c98697c4b99b1ae4da98062271ca3b1
Donchess1 <stepabod@yahoo.com>
2023-10-17 09:41:51 +0000

    day

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,17 +1,43 @@
 #include <stdlib.h>
-
+/**
+ * add - adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: sum of a and b
+ **/
 int add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: product of a and b
+ **/
 int mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * sub - subtracts two integers
+ * @a: integer
+ * @b: integer
+ * Return: difference of a and b
+ **/
 int sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * divi - divides two integers
+ * @a: integer
+ * @b: integer
+ * Return: result of a divided by b, or -1 on division by zero
+ **/
 int divi(int a, int b)
 {
 	if (b == 0)
@@ -20,6 +46,12 @@ int divi(int a, int b)
 	}
 	return (a / b);
 }
+/**
+ * mod - calculates the remainder of division
+ * @a: integer
+ * @b: integer
+ * Return: remainder of a divided by b, or -1 on division by zero
+ **/
 int mod(int a, int b)
 {
 	if (b == 0)
